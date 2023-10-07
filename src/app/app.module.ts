0452fe3864b6103da79cfd844e4e1bfde77de8c9
Stefano Corra <stefano.corra@hotmail.com>
2023-10-07 17:00:31 -0400

    material commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,18 +19,20 @@ import { MatSnackBarModule } from '@angular/material/snack-bar'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSidenavModule,
-    MatgridListModule,
+    MatGridListModule,
     MatMenuModule,
     MatButtonModule,
     MatCardModule,
@@ -41,8 +43,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatTableModule,
     MatBadgeModule,
     MatSnackBarModule,
-
-
   ],
   providers: [],
   bootstrap: [AppComponent]
