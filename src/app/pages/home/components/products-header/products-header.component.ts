a182099380193c247714473a527ed35252c6ba06
Stefano Corra <stefano.corra@hotmail.com>
2023-10-07 22:39:30 -0400

    home component commit

---
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-header',
+  templateUrl: './products-header.components.html' ,
+  styles: [
+  ]
+})
+export class ProductsHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
